067d26ef02db0652ff15baf22a4e77d301875105
Ken Liiv <kenlii@ttu.ee>
2019-02-13 15:02:39 +0200

    Loengud 8-10

---
+++ function-declaration/app.js
@@ -0,0 +1,36 @@
+/*
+function sayHello() {
+    console.log('==============');
+    console.log('hello!');
+    console.log('==============');
+}
+sayHello();
+*/
+//let a = sayHello;
+//a();
+/*
+function sayHello(name) {
+    console.log('==============');
+    console.log('hello ' + name + '!');
+    console.log('==============');
+}
+sayHello('ken');
+*/
+/*
+function calculate(amount) {
+    let result = amount * 5;
+    return result;
+}
+let fResult = calculate(5);
+console.log(fResult);
+*/
+//setTimeout(function () { console.log('2 second wait') }, 2000);
+/* infinite loop
+let counter = 0;
+function timeout() {
+    setTimeout(function () {
+        console.log('hi ' + counter++);
+        timeout();
+    }, 2000);
+}
+*/

1568092a4a17291b3c8b03dcabe740b4f760d2b4
Ken Liiv <kenlii@ttu.ee>
2019-02-14 15:14:45 +0200

    loengud 15-16

---
+++ object/app.js
@@ -0,0 +1,24 @@
+/*
+let car = {
+    make: 'audi',
+    model: 'a4',
+    year: 2020,
+    getPrice: function () {
+        return 5000;
+    },
+    printDescription: function () {
+        console.log(this.year + ' ' + this.make + ' ' + this.model);
+    }
+}
+
+car.printDescription();
+*/
+/*
+var c = {
+    myProperty: [
+        { d: ' this' },
+        { e: 'can' }
+    ]
+}
+*/
+

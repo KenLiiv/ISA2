608f888dd2c7fe537f105a877907adfa4dc7544d
Ken Liiv <kenlii@ttu.ee>
2019-02-25 17:14:59 +0200

    Vastavalt MVC Core juhendile kuni tekib error ning ei saa ASpNetCore.All installida

---
+++ MVC/Controllers/TestController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Core;
+using Facade;
+
+namespace MVC.Controllers
+{
+    public class TestController : Controller
+    {
+		public ActionResult GetView()
+		{
+			var emp = new Employee("Mihkel", "Raud", 20000);
+
+			var vmEmp = new EmployeeViewModel(emp, "Admin");
+			return View("MyView", vmEmp);
+		}
+    }
+}

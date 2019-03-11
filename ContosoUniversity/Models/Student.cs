0e1c8dc8b8128fbde6bd4ed5d0d828c683ee5e03
Ken Liiv <kenlii@ttu.ee>
2019-03-11 17:35:25 +0200

    Inheritance - üleliigse koodi eemaldamine, effektiivsuse tõstmine, lihtsuse tagamine

--- ContosoUniversity/Models/Student.cs
+++ ContosoUniversity/Models/Student.cs
@@ -7,30 +7,13 @@ using System.Threading.Tasks;
 
 namespace ContosoUniversity.Models
 {
-	public class Student
+	public class Student : Person
 	{
-		public int ID { get; set; }
-		[Required]
-		[StringLength(50)]
-		[Display(Name = "Last name")]
-		public string LastName { get; set; }
-		[Required]
-		[StringLength(50)]
-		[Column("FirstName")]
-		[Display(Name = "First name")]
-		public string FirstMidName { get; set; }
 		[DataType(DataType.Date)]
 		[DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
-		[Display(Name = "Enrollment date")]
+		[Display(Name = "Enrollment Date")]
 		public DateTime EnrollmentDate { get; set; }
-		[Display(Name = "Full name")]
-		public string FullName
-		{
-			get
-			{
-				return LastName + ", " + FirstMidName;
-			}
-		}
+
 
 		public ICollection<Enrollment> Enrollments { get; set; }
 	}

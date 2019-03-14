c1c5bb02855c6d1d3eb3f7ee7bc77a387c3e854f
Ken Liiv <kenlii@ttu.ee>
2019-03-14 13:00:14 +0200

    CRUD finish

--- Week07/ContosoUniversity/Pages/Students/Delete.cshtml.cs
+++ Week07/ContosoUniversity/Pages/Students/Delete.cshtml.cs
@@ -20,39 +20,59 @@ namespace ContosoUniversity.Pages.Students
 
         [BindProperty]
         public Student Student { get; set; }
+		public string ErrorMessage { get; set; }
 
-        public async Task<IActionResult> OnGetAsync(int? id)
+		public async Task<IActionResult> OnGetAsync(int? id, bool? saveChangesError = false)
+		{
+			if (id == null)
+			{
+				return NotFound();
+			}
+
+			Student = await _context.Student
+				.AsNoTracking()
+				.FirstOrDefaultAsync(m => m.ID == id);
+
+			if (Student == null)
+			{
+				return NotFound();
+			}
+
+			if (saveChangesError.GetValueOrDefault())
+			{
+				ErrorMessage = "Delete failed. Try again";
+			}
+
+			return Page();
+		}
+
+		public async Task<IActionResult> OnPostAsync(int? id)
         {
             if (id == null)
             {
                 return NotFound();
             }
 
-            Student = await _context.Student.FirstOrDefaultAsync(m => m.ID == id);
+			var student = await _context.Student
+					.AsNoTracking()
+					.FirstOrDefaultAsync(m => m.ID == id);
 
-            if (Student == null)
-            {
-                return NotFound();
-            }
-            return Page();
-        }
+			if (student == null)
+			{
+				return NotFound();
+			}
 
-        public async Task<IActionResult> OnPostAsync(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            Student = await _context.Student.FindAsync(id);
-
-            if (Student != null)
-            {
-                _context.Student.Remove(Student);
-                await _context.SaveChangesAsync();
-            }
-
-            return RedirectToPage("./Index");
-        }
+			try
+			{
+				_context.Student.Remove(student);
+				await _context.SaveChangesAsync();
+				return RedirectToPage("./Index");
+			}
+			catch (DbUpdateException)
+			{
+				return RedirectToAction("./Delete",
+									 new { id, saveChangesError = true });
+			}
+		}
     }
 }
